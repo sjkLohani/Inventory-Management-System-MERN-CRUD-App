5bd80346e3f950021c0617dac446a59cc4112442
ShahrozLohani <shahroz2k21@gmail.com>
2026-07-07 01:30:17 +0500

    Intial commit - MERN Inventory Management System

--- Frontend/inventory_management_system/src/App.js
+++ Frontend/inventory_management_system/src/App.js
@@ -18,21 +18,16 @@ import {
 function App() {
   return (
     <div className="App">
-      <Navbar title="IMS" about="About"></Navbar>
-
       <Router>
+        <Navbar title="IMS" about="About"></Navbar>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/insertproduct" element={<InsertProduct />} />
           <Route path="/updateproduct/:id" element={<UpdateProduct />} />
           <Route path="/about" element={<About />} />
-
         </Routes>
-
       </Router>
-
-
     </div>
   );
 }
--- Frontend/inventory_management_system/src/components/Navbar.js
+++ Frontend/inventory_management_system/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
     <div>
-      <navBar className="navbar navbar-expand-lg bg-danger">
+      <nav className="navbar navbar-expand-lg bg-danger">
         <div className="container-fluid">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -11,13 +12,13 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active text-white fs-4" aria-current="page" href="/">{props.title}</a>
+                <NavLink className="nav-link active text-white fs-4" to="/">{props.title}</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link active text-white fs-4" aria-current="page" href="/products">Products</a>
+                <NavLink className="nav-link active text-white fs-4" to="/products">Products</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link active text-white fs-4" aria-current="page" href="/about">About</a>
+                <NavLink className="nav-link active text-white fs-4" to="/about">About</NavLink>
               </li>
             </ul>
             <form className="d-flex" role="search">
@@ -26,8 +27,7 @@ export default function Navbar(props) {
             </form>
           </div>
         </div>
-
-      </navBar>
+      </nav>
     </div>
   )
 }
